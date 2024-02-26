801af0d769dc0a4bb03d1e8cf423ed23de2b3f63
yasinarafat4870 <yasinarafat4870@gmail.com>
2024-02-26 22:07:30 +0600

    Input button addded done

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+    </>
+  );
+};
+
+export default home;
